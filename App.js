179ff974a460dc41b16a068e577fe4ead0297692
ploykullat <264318241001-st@rmutsb.ac.th>
2023-08-22 11:57:25 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import Reactt from 'react'
+//import ButtonStateDemo from './button-state-demo';
+import TextInputDemo from './text-input-demo';
+    export default function App() {
+      return (
+     //<ButtonDemo/>
+     //<ButtonStateDemo/>
+     <TextInputDemo/>
+  );
+}
